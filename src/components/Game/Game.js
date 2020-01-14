1700a6594f6ed3b8fe67daa37e2940f677fe31d1
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2020-01-13 21:19:03 -0500

    Add scoreboard arrows

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { number, shape, func } from 'prop-types';
 import styles from './Game.module.scss';
 
 const HEADER_HEIGHT = 120;
 
 const Game = ({ height, scoreboard, navTo }) => {
+  const [player1Score, setPlayer1Score] = useState(5);
+  const [player2Score, setPlayer2Score] = useState(8);
   const scoreBoardHeight = height - 1.5 * HEADER_HEIGHT;
   const { player1, player2 } = scoreboard;
 
@@ -12,11 +14,40 @@ const Game = ({ height, scoreboard, navTo }) => {
     <div className={styles.game}>
       <div className={styles.scoreboard} style={{ height: scoreBoardHeight }}>
         <div className={styles.playerScore}>
-          <div className={styles.score}>{player1}</div>
+        <div
+            className={styles.incrementScore}
+            onClick={() => setPlayer1Score(player1Score + 1)}
+          >
+            <div className={styles.arrow}>></div>
+          </div>
+          <div className={styles.score}
+          >
+            {player1Score}
+          </div>
+          <div
+            className={styles.decrementScore}
+            onClick={() => setPlayer1Score(player1Score - 1)}
+          >
+            <div className={styles.arrow}>></div>
+          </div>
         </div>
         <hr style={{ height: scoreBoardHeight - 20 }} />
         <div className={styles.playerScore}>
-          <div className={styles.score}>{player2}</div>
+          <div
+            className={styles.incrementScore}
+            onClick={() => setPlayer2Score(player2Score + 1)}
+          >
+            <div className={styles.arrow}>></div>
+          </div>
+          <div className={styles.score}>
+            {player2Score}
+          </div>
+          <div
+            className={styles.decrementScore}
+            onClick={() => setPlayer2Score(player2Score - 1)}
+          >
+            <div className={styles.arrow}>></div>
+          </div>
         </div>
       </div>
     </div>
